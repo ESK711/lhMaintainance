e189b6ab964418855238a726c82344f3348fc8ab
esk711 <ero@crescent.africa>
2022-06-08 14:16:23 +0300

    server

--- server/server.ts
+++ server/server.ts
@@ -1,64 +1,49 @@
 import * as http from 'http'
 import * as dotenv from 'dotenv'
-import * as session from 'express-session'
-// import * as connectRedis from 'connect-redis'
-import { ApolloServer } from 'apollo-server-express'
-
-import * as cors from 'cors'
 import * as express from 'express'
+import { ApolloServer } from 'apollo-server-express'
 
 // import { frontend } from '../dist/ssr/main'
 import { genSchema } from './utils/genSchema'
-// import { redis } from './utils/redis'
 
-export async function runServer() {
+export async function startServer() {
   dotenv.config()
+
+  let serverStarted = false
+  let displayError: any
+  async function stop(arg0: any) {}
 
   const app = express()
   const httpServer = http.createServer(app)
-  // const RedisStore = connectRedis(session)
-  const port = process.env.PORT || 8000
+  const port = process.env.PORT || 8001
   const host = process.env.HOST
 
   const apolloServer = new ApolloServer({
     schema: genSchema(),
-    introspection: true
+    introspection: true,
+    context: ({ req, res }: any ) => ({ req, res })
   })
 
   app.disable('x-powered-by')
 
-  app.use(cors({
-    origin: '*',
-    credentials: true
-  }))
-
-  // app.set('trust proxy', 1) // trust first proxy
-  app.use(
-    session({
-      // store: new RedisStore({
-        // client: redis
-      // }),
-      name:'uuid',
-      secret: `${ process.env.SESSION_SECRET }`,
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        // path: app,
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        // secure: true,
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days
-      }
-    })
-  )
-
   await apolloServer.start()
   apolloServer.applyMiddleware({ 
     app,
-    // cors: false
+    cors: false
   })
 
-  httpServer.listen(port, () => {
-    console.log(`🚀 Server running on port ${ host }:${ port }`)
-  })
+  if (!serverStarted) {
+    httpServer.listen(port, async () => {
+      try {
+        serverStarted = true
+        console.log(`🚀 Server running on port ${ host }:${ port }`)
+      } catch(err) {
+        displayError = err?.message
+        console.error('SERVER FAILED TO START!')
+        console.error(`ERROR: ${ displayError }`)
+        await new Promise(res => stop(res))
+      }
+    })
+  }
+  
 }
